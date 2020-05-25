8db3b1e56005670885ef69a2a6449cc32fe7b9d9
Robin <robinfuchshofer@gmail.com>
2020-05-25 09:15:39 +0200

    Commit Test

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -1,9 +1,18 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        throw new NotImplementedException();
+        final String regex = "$(''|' ')[0-9].[0-9][0-9]";
+        final String checkString = usCurrencyString;
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(checkString);
+
+        matcher.find();
+        matcher.group();
+
+
+        return matcher;
     }
 }
