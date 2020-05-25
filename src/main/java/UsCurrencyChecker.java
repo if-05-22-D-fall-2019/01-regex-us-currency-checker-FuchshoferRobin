444dbb6e95eac1ce0983ecc12041285b46f8684c
Robin <robinfuchshofer@gmail.com>
2020-05-25 09:29:29 +0200

    All tests run successfully

--- src/main/java/UsCurrencyChecker.java
+++ src/main/java/UsCurrencyChecker.java
@@ -3,15 +3,11 @@ import java.util.regex.Pattern;
 
 public class UsCurrencyChecker {
     public static Matcher getMatcher(String usCurrencyString) {
-        final String regex = "$(''|' ')[0-9].[0-9][0-9]";
+        final String regex = "\\$\\s?\\d+(\\.\\d{1,2})?";
         final String checkString = usCurrencyString;
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(checkString);
-
-        matcher.find();
-        matcher.group();
-
 
         return matcher;
     }
